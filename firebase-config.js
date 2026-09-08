6535e76bc6bce4d9ea2981f8544b643de9ac6f75
hanii3 <s2210520@u.tsukuba.ac.jp>
2026-09-08 14:23:27 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,19 @@
+// Firebaseコンソール →「プロジェクトの設定」→ アプリのfirebaseConfigをここに貼り付けてください。
+// 例:
+// export const firebaseConfig = {
+//   apiKey: "AIza...",
+//   authDomain: "toilet-map-xxxxx.firebaseapp.com",
+//   projectId: "toilet-map-xxxxx",
+//   storageBucket: "toilet-map-xxxxx.appspot.com",
+//   messagingSenderId: "1234567890",
+//   appId: "1:1234567890:web:xxxxxxxxxxxxx"
+// };
+
+export const firebaseConfig = {
+  apiKey: "ここに貼り付け",
+  authDomain: "ここに貼り付け",
+  projectId: "ここに貼り付け",
+  storageBucket: "ここに貼り付け",
+  messagingSenderId: "ここに貼り付け",
+  appId: "ここに貼り付け"
+};

6ad4ac487a61740044bdae9d4ac120fc3770a949
hanii3 <s2210520@u.tsukuba.ac.jp>
2026-09-14 10:01:31 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "toilet-map-v2";
+const ASSETS = [
+  "./toilet-map.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+// ネットワーク優先: まず最新を取りに行き、取れなければキャッシュを使う（オフライン用の保険）
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const resClone = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
+  );
+});
